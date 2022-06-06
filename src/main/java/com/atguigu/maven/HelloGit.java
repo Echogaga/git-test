421f7a34b6376437cfac4018f12d8a94718d1b43
zhangjiajia <1426350146@qq.com>
2022-06-06 12:57:07 +0800

    test hot-fix first commit

--- src/main/java/com/atguigu/maven/HelloGit.java
+++ src/main/java/com/atguigu/maven/HelloGit.java
@@ -3,6 +3,6 @@ package com.atguigu.maven;
 public class HelloGit {
     public static void main(String[] args) {
         System.out.println("hello git");
-        System.out.println();
+        System.out.println("this is branch hot-fix");
     }
 }
